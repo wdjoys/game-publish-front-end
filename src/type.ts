edca0d716e8a58623054232ecfdd71c825a7adde
xiaocao <wdjoys@hotmail.com>
2023-01-30 16:51:28 +0800

    230130

---
+++ src/type.ts
@@ -0,0 +1,16 @@
+/**
+ * @Author: xiaocao
+ * @Date:   2023-01-30 11:22:39
+ * @Last Modified by:   xiaocao
+ * @Last Modified time: 2023-01-30 15:49:02
+ */
+
+type UserInfo = {
+  username: string;
+  password: string;
+  email?: string;
+  phone_number?: string;
+  token?: string;
+};
+
+export type { UserInfo };
