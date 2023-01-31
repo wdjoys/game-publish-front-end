02d12990618bb2a772c538a34aac6ac3a7b95480
xiaocao <wdjoys@hotmail.com>
2023-01-31 23:37:37 +0800

    还得用第三方的虚拟滚动

--- src/components/ad/AdTable.vue
+++ src/components/ad/AdTable.vue
@@ -2,7 +2,7 @@
  * @Author: xiaocao
  * @Date:   2023-01-30 15:00:18
  * @Last Modified by:   xiaocao
- * @Last Modified time: 2023-01-31 17:50:26
+ * @Last Modified time: 2023-01-31 23:36:17
  */
  -->
 
@@ -10,16 +10,18 @@
   <q-virtual-scroll
     class="table"
     type="table"
-    style="max-height: 100%"
-    :virtual-scroll-item-size="48"
+    style="max-height: 80vh"
+    :virtual-scroll-item-size="30"
     :items="AdList"
     v-slot="{ item: row, index }"
   >
-    <tr :key="index">
-      <td>#{{ index }}</td>
-      <td v-for="(col, ii) in row" :key="ii">
-        {{ col }}
-      </td>
+    <tr :key="index" @click="openAd(row[7])">
+      <td>{{ row[1] }}</td>
+      <td>{{ row[2] }}</td>
+      <td>{{ row[3] }}</td>
+      <td>{{ row[4] }}</td>
+      <td>{{ row[5] }}</td>
+      <td>{{ row[6] }}</td>
     </tr>
   </q-virtual-scroll>
 </template>
@@ -28,10 +30,23 @@
 defineProps<{
   AdList: [];
 }>();
+
+const openAd = (url: string) => {
+  window.open(url, '_blank');
+};
 </script>
 
 <style lang="scss" scoped>
 .table {
   background-color: $table-background;
+}
+
+.q-table tbody td {
+  height: 30px;
+  // width: 50%;
+}
+
+.q-table {
+  width: 70%;
 }
 </style>
